85600a7ad9363f4f47e61a4cdbdeae66af4d5cfe
vimm0 <vimmrana0@gmail.com>
2018-11-28 02:14:47 +0545

    fix in main program

--- frontend/linkers/derp.js
+++ frontend/linkers/derp.js
@@ -1,20 +1,30 @@
-// function initDerp() {
+// window.onload = function () {
+//     let {PythonShell} = require('python-shell')
+//     var path = require("path")
 //
 //
+//     var options = {
+//         scriptPath: path.join(__dirname, '/../backend/'),
+//         args : 'runserver 127.0.0.1:8000'
+//     }
+//
+//     var derp = new PythonShell('./manage.py', options);
+//     derp.on('message', function (message) {
+//         console.log(message);
+//     })
 // }
-
-window.onload = function () {
-    let {PythonShell} = require('python-shell')
-    var path = require("path")
-
-
-    var options = {
-        scriptPath: path.join(__dirname, '/../backend/'),
-        args : 'runserver'
-    }
-
-    var derp = new PythonShell('./manage.py', options);
-    derp.on('message', function (message) {
-        console.log(message);
-    })
-}
+// export default function () {
+//     let {PythonShell} = require('python-shell')
+//     var path = require("path")
+//
+//
+//     var options = {
+//         scriptPath: path.join(__dirname, '/../backend/'),
+//         args : 'runserver 127.0.0.1:8000'
+//     }
+//
+//     var derp = new PythonShell('./manage.py', options);
+//     derp.on('message', function (message) {
+//         console.log(message);
+//     })
+// }
